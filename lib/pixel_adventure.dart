94b31184f5013f0f221f8928dfd749a1f6679878
Ray Olivier <rayolivier@outlook.com>
2023-09-08 09:55:59 -0500

    add joystick

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -4,15 +4,18 @@ import 'dart:ui';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/painting.dart';
 import 'package:pixel_adventure/actors/player.dart';
 import 'package:pixel_adventure/levels/level.dart';
 
-class PixelAdventure extends FlameGame with HasKeyboardHandlerComponents {
+class PixelAdventure extends FlameGame
+    with HasKeyboardHandlerComponents, DragCallbacks {
   @override
   Color backgroundColor() => const Color(0xFF211F30);
 
   late final CameraComponent cam;
   Player player = Player(character: 'Mask Dude');
+  late JoystickComponent joystick;
 
   @override
   FutureOr<void> onLoad() async {
@@ -27,6 +30,21 @@ class PixelAdventure extends FlameGame with HasKeyboardHandlerComponents {
 
     addAll([cam, world]);
 
+    addJoystick();
+
     return super.onLoad();
+  }
+
+  void addJoystick() {
+    joystick = JoystickComponent(
+        knob: SpriteComponent(
+            sprite: Sprite(
+          images.fromCache('HUD/Knob.png'),
+        )),
+        background: SpriteComponent(
+            sprite: Sprite(images.fromCache('HUD/Joystick.png'))),
+        margin: const EdgeInsets.only(left: 32, bottom: 32));
+
+    add(joystick);
   }
 }
