89273f2dda3bc9051a989a8c879049aa949518c8
Ray Olivier <rayolivier@outlook.com>
2023-09-27 14:27:29 -0500

    move game inside stateful app with manual web state detection

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/foundation.dart';
@@ -10,22 +12,91 @@ import 'package:pixel_adventure/pixel_adventure.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await is very important for real devices, as it takes time to switch to landscape (but it works without await on emulators)
-  await Flame.device.fullScreen();
-  await Flame.device.setLandscape();
 
-  PixelAdventure game = PixelAdventure();
-  runApp(Container(
-      constraints: const BoxConstraints(
-          maxWidth: 800, minWidth: 300, maxHeight: 500, minHeight: 200),
-      child: GameWidget(
-        game: kDebugMode ? PixelAdventure() : game,
-        overlayBuilderMap: <String,
-            Widget Function(BuildContext, PixelAdventure)>{
-          'gameplayOverlay': (context, game) => GameplayOverlay(game),
-          'mainMenuOverlay': (context, game) => MainMenuOverlay(game: game),
-          'settingsOverlay': (context, game) => SettingsOverlay(game: game),
-          'gameOverOverlay': (context, game) => GameOverOverlay(game: game),
-        },
-      ))); // debug mode refreshes/starts game on changes
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  final PixelAdventure game = PixelAdventure();
+
+  @override
+  didChangeAppLifecycleState(AppLifecycleState state) async {
+    print('The APP state: $state');
+
+    switch (state) {
+      case AppLifecycleState.resumed:
+        print('app resumed');
+        break;
+      case AppLifecycleState.hidden:
+        print('app hidden');
+        break;
+      case AppLifecycleState.inactive:
+        print('app inactive');
+        break;
+      case AppLifecycleState.paused:
+        print('app paused');
+        break;
+      case AppLifecycleState.detached:
+        print('app detached');
+        break;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (kIsWeb) {
+      print('is web!!');
+      window.addEventListener('focus', onFocus);
+      window.addEventListener('blur', onBlur);
+      window.addEventListener('visibilityChange', onVisibilityChange);
+    } else {
+      WidgetsBinding.instance.addObserver(this);
+    }
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    game.musicPlayer.dispose();
+    game.audioPlayer.dispose();
+    super.dispose();
+  }
+
+  void onFocus(Event e) {
+    didChangeAppLifecycleState(AppLifecycleState.resumed);
+  }
+
+  void onBlur(Event e) async {
+    didChangeAppLifecycleState(AppLifecycleState.inactive);
+  }
+
+  void onVisibilityChange(Event e) {
+    didChangeAppLifecycleState(AppLifecycleState.hidden);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        constraints: const BoxConstraints(
+            maxWidth: 800, minWidth: 300, maxHeight: 500, minHeight: 200),
+        child: GameWidget(
+          game: kDebugMode ? PixelAdventure() : game,
+          overlayBuilderMap: <String,
+              Widget Function(BuildContext, PixelAdventure)>{
+            'gameplayOverlay': (context, game) => GameplayOverlay(game),
+            'mainMenuOverlay': (context, game) => MainMenuOverlay(game: game),
+            'settingsOverlay': (context, game) => SettingsOverlay(game: game),
+            'gameOverOverlay': (context, game) => GameOverOverlay(game: game),
+          },
+        ));
+    // throw UnimplementedError();
+  }
 }
